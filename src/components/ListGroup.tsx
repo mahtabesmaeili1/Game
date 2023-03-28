acc1157b1799974adf1025b93bf2cb89816b5074
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-03-28 20:12:26 +0200

    use &&

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,14 +2,12 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  };
+
   return (
     <>
       <h1> List Group</h1>
-      {/* {items.length === 0 ? <p>No item found</p> : null} */}
-      {getMessage()}
+      {items.length === 0 && <p>No item found</p>}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}> {item}</li>
