789ef792e66918dbdd7364d2c0b7bd37f53cfa86
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-03-30 11:34:44 +0200

    to see the event in the consule

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,11 +9,11 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
 
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log("clicked")}
+            onClick={(event) => console.log(event)}
           >
             {" "}
             {item}
