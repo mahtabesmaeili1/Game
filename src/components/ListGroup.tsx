7740bd1ce78a728f88a3e5be1506b008ea517428
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-03-30 11:40:44 +0200

    writing an event handler as a new function then just call it in jsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,11 @@
 // import { Fragment } from "react";
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   //   items = [];
-
+  const handleClick = (event: MouseEvent) => console.log(event);
+  //this is an event handler
+  //we will not add () after the name of handleclick in the down because we r not calling it immediately. calling it is for after clicking.so we will write the name without prantecis
   return (
     <>
       <h1> List Group</h1>
@@ -10,11 +13,7 @@ function ListGroup() {
 
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(event)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {" "}
             {item}
           </li>
